613d34ef62571fd3e1298205f696689990c5c855
ajaypy <ajay.goel@gmail.com>
2020-07-13 22:11:59 -0700

    ratings info

---
+++ ratings-data-service/ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,19 @@
+package com.example.ratingsdataservice.resources;
+
+import com.example.ratingsdataservice.models.Rating;
+import com.example.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+
+}
